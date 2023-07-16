248e5f3bfa2033499825ab708fdd4cbab57dc50a
Erickneitor1416 <erick.munoz@epn.edu.ec>
2023-07-15 20:40:35 -0500

    PAWQ-112: implement Adoption use cases and fixed docker-compose

--- src/Publication/AdoptionPublication/Application/FilterListAdoptionPublicationUseCase.py
+++ src/Publication/AdoptionPublication/Application/FilterListAdoptionPublicationUseCase.py
@@ -1,6 +1,22 @@
-class FilterListAdoptionPublicationUseCase:
-    def __init__(self):
-        self.publication_repository = None
+from datetime import date
+from typing import List
+from Publication.AdoptionPublication.Domain.AdoptionPublication import (
+    AdoptionPublication,
+)
+from Publication.Domain.PublicationRepository import PublicationRepository
 
-    def execute(self, species, date, location, pageNumber, pageSize):
-        pass
+
+class FilterListAdoptionPublicationUseCase:
+    def __init__(self, publication_repository: PublicationRepository):
+        self.publication_repository = publication_repository
+
+    def execute(
+        self, species: str, date: date, location: str, pageNumber: int, pageSize: int
+    ) -> List[AdoptionPublication]:
+        return self.publication_repository.get_by_filters(
+            species=species,
+            date=date,
+            location=location,
+            pageNumber=pageNumber,
+            pageSize=pageSize,
+        )
--- src/Publication/Domain/PublicationRepository.py
+++ src/Publication/Domain/PublicationRepository.py
@@ -1,39 +1,49 @@
 from abc import ABC, abstractmethod
+from datetime import date
+from typing import List
+from Interaction.Comment.Domain.Comment import Comment
+from Interaction.Like.Domain.Like import Like
+
+from Publication.AdoptionPublication.Domain.AdoptionPublication import (
+    AdoptionPublication,
+)
 
 
 class PublicationRepository(ABC):
     @abstractmethod
-    def add_publication(self, publication):
+    def add_publication(self, publication: AdoptionPublication):
         pass
 
     @abstractmethod
-    def get_all(self, pageNumber, pageSize):
+    def get_all(self, pageNumber: int, pageSize: int) -> List[AdoptionPublication]:
         pass
 
     @abstractmethod
-    def get_by_id(self, id):
+    def get_by_id(self, id: int) -> AdoptionPublication:
         pass
 
     @abstractmethod
-    def get_by_filters(self, species, date, location):
+    def get_by_filters(
+        self, species: str, date: date, location: str, pageNumber: int, pageSize: int
+    ) -> List[AdoptionPublication]:
         pass
 
     @abstractmethod
-    def add_like(self, like):
+    def add_like(self, like: Like):
         pass
 
     @abstractmethod
-    def remove_like_by_id(self, like_id):
+    def remove_like_by_id(self, like_id: int):
         pass
 
     @abstractmethod
-    def get_likes_by_pub_id(self, id):
+    def get_likes_by_pub_id(self, id: int) -> List[Like]:
         pass
 
     @abstractmethod
-    def get_comments_by_pub_id(self, id):
+    def get_comments_by_pub_id(self, id: int) -> List[Comment]:
         pass
 
     @abstractmethod
-    def add_comment(self, comment):
+    def add_comment(self, comment: Comment):
         pass
