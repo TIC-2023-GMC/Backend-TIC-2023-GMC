03fa8d7bc2545136890cf6a5eb1e19f3db692fcf
Gandhy <gandhy.garcia@epn.edu.ec>
2023-07-18 19:04:03 -0500

    PAWQ-111: Add environment variable for firebase account key file path

--- src/Photo/Infrastructure/firebase/firebase_config.py
+++ src/Photo/Infrastructure/firebase/firebase_config.py
@@ -1,15 +1,13 @@
 import firebase_admin
 from firebase_admin import credentials
 
-# import os
+import os
 
 
 def initialize_firebase():
-    # account_key_path = os.getenv("FIREBASE_ACCOUNT_KEY_PATH")
+    account_key_path = os.getenv("FIREBASE_ACCOUNT_KEY_PATH")
     # Load Firebase credentials from the JSON file
-    cred = credentials.Certificate(
-        "C:/Users/usuario/Desktop/Firebase_Key/pawq-fc6dc-firebase-adminsdk-3tf6e-aec938493d.json"
-    )
+    cred = credentials.Certificate(account_key_path)
 
     # Initialize the Firebase Admin SDK
     app = firebase_admin.initialize_app(
