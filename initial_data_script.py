a5d6f0fa158233430f7ab853a5b556971769520d
Gandhy <gandhy.garcia@epn.edu.ec>
2023-07-18 19:23:20 -0500

    PAWQ-111: Change initial_data_script to have the user inserted in the publications

--- initial_data_script.py
+++ initial_data_script.py
@@ -48,31 +48,13 @@ users_data = {
 
 users_collection.insert_one(users_data)
 
+user = users_collection.find_one({"email": "gandhygarcia@outlook.es"})
+
 adoption_publications_collection = db["adoption_publications"]
 
 adoption_publications_data = [
     {
-        "user": {
-            "first_name": "Gandhy",
-            "last_name": "García",
-            "mobile_phone": "0983473043",
-            "neighborhood": "Cumbayá",
-            "email": "gandhygarcia@outlook.es",
-            "password": "password123",
-            "num_previous_pets": 2,
-            "num_current_pets": 1,
-            "outdoor_hours": 6,
-            "house_space": 100,
-            "has_yard": False,
-            "main_pet_food": "Casera",
-            "pet_expenses": 40.5,
-            "motivation": "Amor por los animales",
-            "favorite_adoption_publications": [],
-            "photo": {
-                "photo_id": 2,
-                "img_path": "https://scontent.fgye1-1.fna.fbcdn.net/v/t1.6435-9/74242360_3195954163812838_4274861617784553472_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFRCjYsTZuQlf2PHyTPJ3HYymegSJbxrSjKZ6BIlvGtKPYIzlm5LEqBr9cR0tDl-FEvtHfkBqZQ6LHCgw-pkTlW&_nc_ohc=dye6H3TWD6QAX-v2xOF&_nc_ht=scontent.fgye1-1.fna&oh=00_AfA1PhUUa4W64PFDg1Z1mU0IoNDXPm-AHLagWXq2astHww&oe=64D8F6A2",
-            },
-        },
+        "user": user,
         "description": "Hermoso gato de 3 meses busca un hogar",
         "publication_date": "2023-07-13",
         "photo": {
@@ -91,27 +73,7 @@ adoption_publications_data = [
         "vaccination_card": False,
     },
     {
-        "user": {
-            "first_name": "Gandhy",
-            "last_name": "García",
-            "mobile_phone": "0983473043",
-            "neighborhood": "Cumbayá",
-            "email": "gandhygarcia@outlook.es",
-            "password": "password123",
-            "num_previous_pets": 2,
-            "num_current_pets": 1,
-            "outdoor_hours": 6,
-            "house_space": 100,
-            "has_yard": False,
-            "main_pet_food": "Casera",
-            "pet_expenses": 40.5,
-            "motivation": "Amor por los animales",
-            "favorite_adoption_publications": [],
-            "photo": {
-                "photo_id": 2,
-                "img_path": "https://scontent.fgye1-1.fna.fbcdn.net/v/t1.6435-9/74242360_3195954163812838_4274861617784553472_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFRCjYsTZuQlf2PHyTPJ3HYymegSJbxrSjKZ6BIlvGtKPYIzlm5LEqBr9cR0tDl-FEvtHfkBqZQ6LHCgw-pkTlW&_nc_ohc=dye6H3TWD6QAX-v2xOF&_nc_ht=scontent.fgye1-1.fna&oh=00_AfA1PhUUa4W64PFDg1Z1mU0IoNDXPm-AHLagWXq2astHww&oe=64D8F6A2",
-            },
-        },
+        "user": user,
         "description": "Perrita de 1 año busca un hogar",
         "publication_date": "2023-07-14",
         "photo": {
