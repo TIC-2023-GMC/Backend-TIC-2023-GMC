3de81b68297060ed7350be6de7b6474a40c0bf82
Gandhy <gandhy.garcia@epn.edu.ec>
2023-07-18 22:23:26 -0500

    PAWQ-111: Fix Black formatting

--- src/Photo/Infrastructure/firebase/firebase_config.py
+++ src/Photo/Infrastructure/firebase/firebase_config.py
@@ -1,16 +1,17 @@
-import pyrebase 
+import pyrebase
 import os
 
 firebaseConfig = {
-  "apiKey": os.getenv("API_KEY"),
-  "authDomain": os.getenv("AUTH_DOMAIN"),
-  "projectId": os.getenv("PROJECT_ID"),
-  "storageBucket": os.getenv("STORAGE_BUCKET"),
-  "messagingSenderId": os.getenv("MESSAGING_SENDER_ID"),
-  "appId": os.getenv("APP_ID"),
-  "measurementId": os.getenv("MEASUREMENT_ID"),
-  "databaseURL": os.getenv("DATABASE_URL")
+    "apiKey": os.getenv("API_KEY"),
+    "authDomain": os.getenv("AUTH_DOMAIN"),
+    "projectId": os.getenv("PROJECT_ID"),
+    "storageBucket": os.getenv("STORAGE_BUCKET"),
+    "messagingSenderId": os.getenv("MESSAGING_SENDER_ID"),
+    "appId": os.getenv("APP_ID"),
+    "measurementId": os.getenv("MEASUREMENT_ID"),
+    "databaseURL": os.getenv("DATABASE_URL"),
 }
+
 
 def initialize_firebase():
     firebase = pyrebase.initialize_app(firebaseConfig)
