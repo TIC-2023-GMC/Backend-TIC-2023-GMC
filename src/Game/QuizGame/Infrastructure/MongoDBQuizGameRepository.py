81f833965467570b5cfd2d82698aef2641a0090e
Gandhy <gandhy.garcia@epn.edu.ec>
2023-08-02 00:18:15 -0500

    PAWQ-89: Refactor get_game and save_game functions in MongoDBQuizGameRepository

--- src/Game/QuizGame/Infrastructure/MongoDBQuizGameRepository.py
+++ src/Game/QuizGame/Infrastructure/MongoDBQuizGameRepository.py
@@ -21,8 +21,6 @@ class MongoDBQuizGameRepository(QuizGameRepository):
         game_dict["user_id"] = ObjectId(game_dict["user_id"])
         existing_game = self.game_quiz.find_one({"user_id": game_dict["user_id"]})
         if existing_game is None:
-            game_dict["_id"] = ObjectId()
-            game_dict["user_id"] = ObjectId(game_dict["user_id"])
             self.game_quiz.insert_one(game_dict)
             was_created = True
         else:
@@ -33,32 +31,21 @@ class MongoDBQuizGameRepository(QuizGameRepository):
         return was_created
 
     def get_game(self, user_id: str) -> QuizGame:
-        game = self.game_quiz.find_one({"user_id": ObjectId(user_id)})
-        if game:
-            game["_id"] = str(game["_id"])
-            game["user_id"] = str(game["user_id"])
-            game = QuizGameFactory.create_game(**game)
+        existing_game = self.game_quiz.find_one({"user_id": ObjectId(user_id)})
+        subset_size = 2
+        game_questions = []
+        questions = self.questions_quiz.aggregate([{"$sample": {"size": subset_size}}])
+        for question in questions:
+            question.pop("_id", None)
+            game_questions.append(Question(**question))
+
+        if existing_game:
+            existing_game["_id"] = str(existing_game["_id"])
+            existing_game["user_id"] = str(existing_game["user_id"])
+            existing_game["game_questions"] = game_questions
+            existing_game = QuizGameFactory.create_game(**existing_game)
         else:
-            subset_size = 3
-
-            """ questions = self.questions_quiz.find()
-            game_questions = []
-            if questions:
-                for question in questions:
-                    question.pop("_id", None)
-                    game_questions.append(Question(**question))
-                random.shuffle(game_questions) """
-
-            questions = self.questions_quiz.aggregate(
-                [{"$sample": {"size": subset_size}}]
-            )
-            game_questions = []
-            if questions:
-                for question in questions:
-                    question.pop("_id", None)
-                    game_questions.append(Question(**question))
-
-            game = QuizGameFactory.create_game(
+            existing_game = QuizGameFactory.create_game(
                 _id=None,
                 game_name="QuizGame",
                 game_description="",
@@ -69,5 +56,5 @@ class MongoDBQuizGameRepository(QuizGameRepository):
                 game_time=0,
                 user_id=user_id,
             )
-            self.save_game(game)
-        return game
+        self.save_game(existing_game)
+        return existing_game
